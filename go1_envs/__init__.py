a57cb3421910f5974575c0033a7e85960450d755
wpjNB <2524890186.com>
2026-05-13 21:39:57 +0800

    feat: register Go1Balance-v0, Go1Walk-v0, Go1Jump-v0 envs

--- go1_envs/__init__.py
+++ go1_envs/__init__.py
@@ -1,1 +1,21 @@
 """Go1 custom Gymnasium environments for reinforcement learning."""
+
+from gymnasium.envs.registration import register
+
+register(
+    id="Go1Balance-v0",
+    entry_point="go1_envs.go1_balance:Go1BalanceEnv",
+    max_episode_steps=1000,
+)
+
+register(
+    id="Go1Walk-v0",
+    entry_point="go1_envs.go1_walk:Go1WalkEnv",
+    max_episode_steps=1000,
+)
+
+register(
+    id="Go1Jump-v0",
+    entry_point="go1_envs.go1_jump:Go1JumpEnv",
+    max_episode_steps=1000,
+)
